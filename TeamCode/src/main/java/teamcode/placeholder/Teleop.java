25ba0b8fc1a8fe57529d87460cadbca9033eb32f
dragonher0 <142940920+dragonher0@users.noreply.github.com>
2025-07-28 00:10:49 -0400

    broken

--- TeamCode/src/main/java/teamcode/placeholder/Teleop.java
+++ TeamCode/src/main/java/teamcode/placeholder/Teleop.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-    public class FieldCentricOpmode extends LinearOpMode() {
+public class Teleop extends LinearOpMode {
     //Declare vars
     public  DcMotor frontLeft;
     public DcMotor frontRight;
@@ -20,68 +20,68 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
     //change this var based on whether u want the claw open or closed when u start the program
     public boolean intakeToggle = false;
 
-     public void runOpMode() {
-     if (opmodeInInit) {
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        clawServo = hardwareMap.get(Servo.class, "intakeClaw");
-        // ur IMU code or wtvr, sry i dont feel like copy pasting it here
-        IMU imu = hardwareMap.get(IMU.class, "imu");
-        // Adjust the orientation parameters to match your robot
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-        imu.initialize(parameters);
+    @Override()
+    public void runOpMode() {
+        if (opModeInInit()) {
+            frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+            frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+            backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+            backRight = hardwareMap.get(DcMotor.class, "backRight");
+            clawServo = hardwareMap.get(Servo.class, "intakeClaw");
+            // ur IMU code or wtvr, sry i dont feel like copy pasting it here
+            IMU imu = hardwareMap.get(IMU.class, "imu");
+            // Adjust the orientation parameters to match your robot
+            IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                    RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                    RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+            // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+            imu.initialize(parameters);
         }
         waitForStart();
-        while (opModeIsActive) {
-        // code for the drive, sry im lazy
-        y = gamepad1.left_stick_y;
-        x = gamepad1.left_stick_x;
-        rx = gamepad1.right_stick_x;
+        while (opModeIsActive()) {
+            // code for the drive, sry im lazy
+            y = gamepad1.left_stick_y;
+            x = gamepad1.left_stick_x;
+            rx = gamepad1.right_stick_x;
 
-        frontLeft.setPower(//power);
-                //etc
-        if (gamepad1.options) {
-            IMU.resetYaw();
-        }
+            frontLeft.setPower(//power);
+                    //etc
+            if (gamepad1.options) {
+                IMU.resetYaw();
+            }
 
-        double botHeading = IMU.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            double botHeading = IMU.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-        // Rotate the movement direction counter to the bot's rotation
-        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
-        rotX = rotX * 1.1;  // Counteract imperfect strafing
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
 
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio,
-        // but only if at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-        double frontLeftPower = (rotY + rotX + rx) / denominator;
-        double backLeftPower = (rotY - rotX + rx) / denominator;
-        double frontRightPower = (rotY - rotX - rx) / denominator;
-        double backRightPower = (rotY + rotX - rx) / denominator;
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
 
-        frontLeft.setPower(frontLeftPower);
-        backLeft.setPower(backLeftPower);
-        frontRight.setPower(frontRightPower);
-        backRight.setPower(backRightPower);
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
 
 
-        if (gamepad1.circleWasPressed()) {
-            intakeToggle = !intakeToggle;
-        }
-        if (intakeToggle) {
-            clawServo.setPosition(/*position*/);
-        } else {
-            clawServo.setPosition(/*position*/);
+            if (gamepad1.circleWasPressed()) {
+                intakeToggle = !intakeToggle;
+            }
+            if (intakeToggle) {
+                clawServo.setPosition(/*position*/);
+            } else {
+                clawServo.setPosition(/*position*/);
+            }
         }
     }
- }
 
 }
-
